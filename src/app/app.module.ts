7f8316751ab44e3ed0cd8e89fb30f3866972ba00
Vladan Matorkić <vladan.matorkic.96@gmail.com>
2022-10-05 14:57:57 +0200

    first upload commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistracionComponent } from './registracion/registracion.component';
+
+const appRoutes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'registracion', component: RegistracionComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistracionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registracion/registracion.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-registracion',
+  templateUrl: './registracion.component.html',
+  styleUrls: ['./registracion.component.css']
+})
+export class RegistracionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  floatingName: string = "";
+  surname: string = "";
+  email: string = "";
+  password: string = "";
+
+  async saveData(){
+    const person = {
+      name: this.floatingName,
+      lastName: this.surname,
+      email: this.email,
+      password: this.password
+    }
+
+    const jsonData = JSON.stringify(person, null, 2);
+    // console.log(jsonData);
+    // localStorage.setItem('user', jsonData);
+    const localStorageContent = localStorage.getItem('users');
+
+    let users;
+
+    if(localStorageContent === null) {
+      users = [];
+    } else {
+      users = JSON.parse(localStorageContent);
+    }
+
+    users.push(jsonData);
+
+    localStorage.setItem('users', JSON.stringify(users, null, 2));
+  }
+}
