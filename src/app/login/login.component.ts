5f5a69064a8abc9d7c8829818268463db7830192
Vladan Matorkić <vladan.matorkic.96@gmail.com>
2022-10-17 21:19:33 +0200

    Login update
    Creating login from local storage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,33 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  email: string = "";
+  password: string = "";
+
+  login(){
+    const user = {
+      email: this.email,
+      password: this.password
+    }
+
+    const localStorageContent = localStorage.getItem('users');
+    let users;
+
+    if (localStorageContent === null) {
+      alert("Ne postoji taj korisnik!");
+    } else {
+      users = JSON.parse(localStorageContent);
+    }
+
+    users.forEach((u: { email: string; password: string; }) => {
+      if (u.email == this.email && u.password == this.password) {
+        alert("Postoji korisnik!");
+      } else {
+        alert("Nije tacan email ili sifra!");
+      }
+    });
   }
 
 }
