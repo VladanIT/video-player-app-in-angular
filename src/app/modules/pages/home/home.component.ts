a82a362f8287e82e18054543fe55fcce8519ba58
Vladan Matorkić <vladan.matorkic.96@gmail.com>
2022-10-22 20:15:16 +0200

    Display all videos on home page
    Displaying all videos on home page, html and css logic of displaying, getting data from local storage of all videos and displaying them

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'videos',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
+  videos: any;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit(){
+
+    const localStorageContent = localStorage.getItem('videos');
+
+
+    if (localStorageContent === null) {
+      alert("ne postoji ni jedan video");
+    } else {
+      this.videos = JSON.parse(localStorageContent);
+    }
+
+    console.log(this.videos)
+
+    return this.videos;
   }
 
 }
